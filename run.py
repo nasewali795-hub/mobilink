d8d5f7e90384136f5a1ebad6b41d3cb6320a754f
ryanspacecodingacademy-cyber <ryanspacecodingacademy@gmail.com>
2026-09-11 03:51:28 +0200

    Remove old Flask code and prepare FastAPI for deployment

--- run.py
+++
@@ -1,7 +0,0 @@
-from app import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    print("Starting Microfinance Hierarchical API on http://127.0.0.1:5000 ...")
-    app.run(debug=True, port=5000)
